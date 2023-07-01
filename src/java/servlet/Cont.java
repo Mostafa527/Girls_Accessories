d1dced4dd11a0e58e6e9162f5b42bae10b292b53
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-07-01 11:51:21 +0200

    Create new action (list) to get all accessories from database and show it

--- src/java/servlet/Cont.java
+++ src/java/servlet/Cont.java
@@ -1,12 +1,16 @@
 
 package servlet;
 
+import databaseIO.TestAccessoryDB;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Accessory;
 
 /**
  *
@@ -17,55 +21,29 @@ public class Cont extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Cont</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Cont at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+               response.setContentType("text/html;charset=UTF-8");
+               String url = "";
+               String msg = "";
+               String code = request.getParameter("code");
+
+               HttpSession sess = request.getSession();
+
+                if(code.equalsIgnoreCase("list")){
+                    ArrayList<Accessory> all = TestAccessoryDB.getDatabaseTable();
+                    sess.setAttribute("all", all);
+                    url = "/list_table.jsp";
+                    msg=  "listing table from database";
+                    sess.setAttribute("msg", msg);
+            }
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+  
     @Override
     public String getServletInfo() {
         return "Short description";
