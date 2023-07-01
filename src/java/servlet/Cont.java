432e45842a8305c54f1d34e0d1303ad39f0ebea1
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-07-01 12:07:50 +0200

    1-Update Cont and add new line getServletContext to make forward operation 2-Create new jsp page (list_table) to view all accessories list

--- src/java/servlet/Cont.java
+++ src/java/servlet/Cont.java
@@ -1,7 +1,8 @@
-
+    
 package servlet;
 
 import databaseIO.TestAccessoryDB;
+import static databaseIO.TestAccessoryDB.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -12,41 +13,47 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Accessory;
 
-/**
- *
- * @author Mostafa.Net
- */
+
+
 public class Cont extends HttpServlet {
 
-
+ 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-               response.setContentType("text/html;charset=UTF-8");
-               String url = "";
-               String msg = "";
-               String code = request.getParameter("code");
+        response.setContentType("text/html;charset=UTF-8");
+            String url = "";
+            String msg = "";
+            String code = request.getParameter("code");
 
-               HttpSession sess = request.getSession();
-
-                if(code.equalsIgnoreCase("list")){
-                    ArrayList<Accessory> all = TestAccessoryDB.getDatabaseTable();
-                    sess.setAttribute("all", all);
-                    url = "/list_table.jsp";
-                    msg=  "listing table from database";
-                    sess.setAttribute("msg", msg);
+            HttpSession sess = request.getSession();
+            if(code.equalsIgnoreCase("list")){
+                ArrayList<Accessory> all = TestAccessoryDB.getDatabaseTable();
+                sess.setAttribute("all", all);
+                url = "/list_table.jsp";
+                msg=  "listing table from database";
+                sess.setAttribute("msg", msg);
             }
+         
+                     
+            this.getServletContext().getRequestDispatcher(url).forward(request, response);
         }
-    
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-  
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
 }
+
---
+++ web/list_table.jsp
@@ -0,0 +1,52 @@
+
+<%@page import="java.util.ArrayList"%>
+<%@page import="model.* , databaseIO.*"%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Accessories Project</title>
+    </head>
+    <body>
+       
+        <%
+            HttpSession sess = request.getSession();
+            ArrayList<Accessory> al = (ArrayList<Accessory>) sess.getAttribute("all");
+            String msg = (String) sess.getAttribute("msg");
+            
+       %>
+    
+       
+      
+       <table border="2" width="5" cellspacing="5" cellpadding="5">
+           <thead>
+               <tr>
+                   <th>Accessory Number</th><th>Accessory Name</th><th>Brand</th>
+                   <th>Color</th><th>Quantity</th><th>Price</th><th>Picture</th>
+               </tr>
+           </thead>
+           <tbody>
+               <% for (int i=0;i<al.size();i++){ %>
+               <tr>
+                   <td><%=al.get(i).getAnum()%></td><td><%=al.get(i).getAname()%></td>
+                   <td><%=al.get(i).getAbrand()%></td> <td><%=al.get(i).getAcolor()%></td>
+                   <td><%=al.get(i).getAquantity()%></td> <td><%=al.get(i).getAprice()%></td>
+                   <td><img src=<%= "pics/"+al.get(i).getAnum()+".jpg" %> width="100" height="100" onerror="this.src='pics/accessory.jpg';"/>
+                          <form action="Kuwait" method="get">
+                              <input type="hidden" name="anum" value=<%=al.get(i).getAnum() %> ><br>
+                              <input type="submit" name="code" value="AddToCart"><br>                              
+                       
+                          </form><br>
+                           </td>
+               </tr>
+               <% } %>
+           </tbody>
+       </table>
+           <a href ="index.html"> Home </a></div>
+    </body>
+    
+    
+
+</html>
